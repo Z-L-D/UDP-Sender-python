20417334c5aa566addccb6f1cd195d2cf874cf92
Z-L-D <zacldoyle@gmail.com>
2022-06-13 18:45:37 -0500

    -

--- program.py
+++ program.py
@@ -4,6 +4,5 @@ command = b"Hello from Python"
 address = "127.0.0.1"
 port = 5500
 
-socket_object = socket.socket(socket.AF_INET,
-                     socket.SOCK_DGRAM)
+socket_object = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 socket_object.sendto(command, (address, port))
